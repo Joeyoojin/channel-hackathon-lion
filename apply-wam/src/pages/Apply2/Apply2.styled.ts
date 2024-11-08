ab65c5c6bff70a5f49bcf24f57f16c5966477b31
조유진 <uzinfamily@snu.ac.kr>
2024-11-09 03:22:14 +0900

    feat: apply-wam publishing

---
+++ apply-wam/src/pages/Apply2/Apply2.styled.ts
@@ -0,0 +1,108 @@
+/** @jsxImportSource @emotion/react */
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+  width: 385px;
+  height: 686px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 30px;
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  gap: 43px;
+`
+
+export const Header = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10px;
+`
+
+export const Title = styled.div`
+  color: #ff7b00;
+  font-size: 30px;
+  font-weight: 900;
+  font-family: 'Noto Sans KR', sans-serif;
+`
+
+export const QuestionContainer = styled.div`
+  width: 95%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`
+
+export const QuestionHeader = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  color: #ff7b00;
+  font-family: 'Noto Sans KR', sans-serif;
+`
+
+export const QuestionNumber = styled.span`
+  color: black;
+  font-size: 15px;
+  font-weight: bold;
+`
+
+export const QuestionText = styled.span`
+  font-size: 15px;
+  font-weight: bold;
+  color: #ff7b00;
+  font-family: 'Noto Sans KR', sans-serif;
+`
+
+export const TextArea = styled.textarea`
+  width: 100%;
+  height: 115px;
+  padding: 12px;
+  border-radius: 15px;
+  border: 1px solid #ff7b00;
+  outline: none;
+  resize: none;
+  font-weight: 500;
+  font-family: 'Noto Sans KR', sans-serif;
+
+  &:focus {
+    border-color: #e06a00;
+  }
+`
+
+export const PasswordInput = styled.input`
+  width: 100%;
+  padding: 12px;
+  border-radius: 15px;
+  border: 1px solid #ff7b00;
+  outline: none;
+  font-family: 'Noto Sans KR', sans-serif;
+
+  &:focus {
+    border-color: #e06a00;
+  }
+`
+
+export const SubmitButton = styled.button`
+  width: 100%;
+  height: 46px;
+  padding: 10px;
+  background-color: #ff7b00;
+  border-radius: 10px;
+  color: white;
+  font-size: 18px;
+  font-weight: 600;
+  font-family: 'Noto Sans KR', sans-serif;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e06a00;
+  }
+
+  &:active {
+    background-color: #cc5d00;
+  }
+`
