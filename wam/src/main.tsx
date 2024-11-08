bdbfd9a638c14fc70a78fb77176f8077badbb70d
Taehyeungkim <kth1837@snu.ac.kr>
2024-11-09 01:50:08 +0900

    initial setting

--- wam/src/main.tsx
+++ wam/src/main.tsx
@@ -5,3 +5,63 @@ import '@channel.io/bezier-react/styles.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+
+class ChannelIOWam {
+  getWamData(key: string) {
+    if (!key) return null
+    return key
+  }
+  close({
+    appId,
+    name,
+    params,
+  }: { appId: string; name: string; params: Record<string, any> } | undefined) {
+    console.log('close')
+  }
+
+  setSize({ width, height }: { width: number; height: number }) {
+    console.log('set size')
+  }
+
+  callFunction({
+    appId,
+    name,
+    params,
+  }: {
+    appId: string
+    name: string
+    params: Record<string, any>
+  }) {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(true), 1000)
+    })
+  }
+
+  callNativeFunction({
+    name,
+    params,
+  }: {
+    name: string
+    params: Record<string, any>
+  }) {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(true), 1000)
+    })
+  }
+
+  callCommand({
+    appId,
+    name,
+    params,
+  }: {
+    appId: string
+    name: string
+    params: Record<string, any>
+  }) {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(true), 1000)
+    })
+  }
+}
+
+window.ChannelIOWam = new ChannelIOWam()
